121df327b1ea7bb7957b165e72311f18c1201c2e
User <cer00@protonmail.com>
2025-03-28 14:20:48 +0300

    First Commit

---
+++ resources/views/layouts/js.blade.php
@@ -0,0 +1,18 @@
+<script src="{{asset('MainPublic/assets/js/vendor/jquery-3.7.0.min.js')}}"></script>
+<script src="{{asset('MainPublic/assets/js/vendor/jquery-migrate-3.3.0.min.js')}}"></script>
+<script src="{{asset('MainPublic/assets/js/vendor/bootstrap.bundle.min.js')}}"></script>
+<!--Other-->
+<script src="{{asset('MainPublic/assets/js/plugins/magnific-popup.js')}}"></script>
+<script src="{{asset('MainPublic/assets/js/plugins/perfect-scrollbar.min.js')}}"></script>
+<script src="{{asset('MainPublic/assets/js/plugins/swiper-bundle.min.js')}}"></script>
+<script src="{{asset('MainPublic/assets/js/plugins/slick.js')}}"></script>
+<script src="{{asset('MainPublic/assets/js/plugins/jquery.carouselTicker.js')}}"></script>
+<script src="{{asset('MainPublic/assets/js/plugins/masonry.min.js')}}"></script>
+<script src="{{asset('MainPublic/assets/js/plugins/scrollup.js')}}"></script>
+<script src="{{asset('MainPublic/assets/js/plugins/wow.js')}}"></script>
+<script src="{{asset('MainPublic/assets/js/plugins/waypoints.js')}}"></script>
+<script src="{{asset('MainPublic/assets/js/plugins/counterup.js')}}"></script>
+<!-- Count down-->
+<script src="{{asset('MainPublic/assets/js/vendor/jquery.countdown.min.js')}}"></script>
+<!--Custom script for this template-->
+<script src="{{asset('MainPublic/assets/js/main.js?v=1.0.0')}}"></script>
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,105 @@
+<header class="header sticky-bar">
+    <div class="container">
+        <div class="main-header">
+            <div class="header-left">
+                <div class="header-logo"><a class="d-flex" href="/l"><img alt="PF" src="{{asset('MainPublic/assets/imgs/template/logo.svg')}}"></a></div>
+                <div class="header-nav">
+                    <nav class="nav-main-menu d-none d-xl-block">
+                        <ul class="main-menu">
+
+                            <li class="mega-li has-children"><a href="#">{{__('Проекты')}}</a>
+                                <div class="mega-menu">
+                                    <div class="mega-menu-inner">
+                                        <div class="row">
+                                            <div class="col-lg-8">
+                                                <div class="row">
+                                                    <div class="col-lg-3">
+                                                        <h6>{{__('Проекты')}}</h6>
+                                                        <ul>
+                                                            <li><a href="#">{{__('Мегапроекты')}}</a></li>
+                                                            <li><a href="#">{{__('ГЧП')}}</a></li>
+                                                            <li><a href="#">{{__('Стартапы')}}</a></li>
+                                                            <li><a href="#">{{__('Проектное ICO')}}</a></li>
+                                                        </ul>
+                                                    </div>
+                                                    <div class="col-lg-3">
+                                                        <h6>{{__('Экспертиза')}}</h6>
+                                                        <ul>
+                                                            <li><a href="#">{{__('AI Экспертиза')}}</a></li>
+                                                            <li><a href="#">{{__('Эксперты')}}</a></li>
+                                                        </ul>
+                                                    </div>
+                                                    <div class="col-lg-3">
+                                                        <h6>{{__('Планы участия')}}</h6>
+                                                        <ul>
+                                                            <li><a href="#">{{__('Сейф')}}</a></li>
+                                                            <li><a href="#">{{__('Вексель')}}</a></li>
+                                                            <li><a href="#">{{__('Раунд')}}</a></li>
+                                                            <li><a href="#">{{__('Взнос')}}</a></li>
+                                                        </ul>
+                                                    </div>
+                                                    <div class="col-lg-3">
+                                                        <h6>{{__('Рейтинг')}}</h6>
+                                                        <ul>
+                                                            <li><a href="#">{{__('ТОП-Проектов')}}</a></li>
+                                                            <li><a href="#">{{__('ТОП-Инвесторов')}}</a></li>
+
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-4">
+                                                <div class="box-desc-menu">
+                                                    <h6 class="text-16-semibold">{{__('Предложить свой проект для привлечения инвестирования')}}</h6>
+                                                    <p class="text-xs mt-10 mb-25">проекты в секторах: ИТ, Интернет, Мобильные, Наука о жизни и здоровье,Высокотехнологичное оборудование и новые материалы
+                                                        , Чистое и бережливое производство, энергосбережение...</p>
+                                                    <a class="btn btn-black-md btn-rounded" href="#">{{__('К заявке')}}
+                                                        <svg width="22" height="8" viewbox="0 0 22 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                            <path d="M22 4.00032L18.4791 0.479492V3.3074H0V4.69333H18.4791V7.52129L22 4.00032Z" fill=""></path>
+                                                        </svg></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="has-children"><a href="#">{{__('ЦФА')}}</a>
+                                <ul class="sub-menu">
+                                    <li><a href="#">{{__('УЦП')}}</a></li>
+                                    <li><a href="#">{{__('Цифровой М2')}}</a></li>
+                                    <li><a href="#">{{__('Контроль 115-ФЗ')}}</a></li>
+                                    <li><a href="#">{{__('Предложения')}}</a></li>
+                                </ul>
+                            </li>
+                            <li class="has-children"><a href="#">{{__('Исламский аккаунт')}}</a></li>
+                            <li class="has-children"><a href="#">{{__('Маркетплейс')}}</a></li>
+                            <li class="has-children"><a href="#">{{__('Сервисы')}}</a>
+                                <ul class="sub-menu">
+                                    <li><a href="#">{{__('API')}}</a></li>
+                                    <li><a href="#">{{__('PFMobile')}}</a></li>
+                                </ul>
+                            </li>
+                            <li class="has-children"><a href="#">{{__('База знаний')}}</a></li>
+
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="header-right"><a class="btn btn-search hover-up" href="#"></a>
+                <div class="form-search p-20 dark">
+                    <form action="#">
+                        <input class="form-control" type="text" placeholder="{{__('Поиск')}}">
+                        <input class="btn-search-2" type="submit" value="">
+                    </form>
+                    <div class="popular-keywords text-start mt-20">
+                        <p class="mb-10 color-white">{{__('Популярные запросы')}}:</p><a class="color-gray-600 mr-10 font-xs" href="#"># платформа</a><a class="color-gray-600 mr-10 font-xs" href="#"># Database</a><a class="color-gray-600 mr-10 font-xs" href="#"># категория</a>
+                    </div>
+                </div><a class="btn btn-brand-4-medium hover-up" href="#">{{__('Присоединиться')}}
+                    <svg width="22" height="22" viewbox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M22 11.0003L18.4791 7.47949V10.3074H0V11.6933H18.4791V14.5213L22 11.0003Z" fill=""></path>
+                    </svg></a>
+                <div class="burger-icon burger-icon-white"><span class="burger-icon-top"></span><span class="burger-icon-mid"></span><span class="burger-icon-bottom"></span></div>
+            </div>
+        </div>
+    </div>
+</header>
